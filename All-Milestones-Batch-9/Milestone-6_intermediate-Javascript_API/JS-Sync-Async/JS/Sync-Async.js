75d1767a5eb7dd83aad3a509aae1c471e3f3d97f
farhan-mazumder <farhanmazumder75@gmail.com>
2024-03-05 16:01:32 +0600

    module-35 practices about anynchronous JS has been added.

--- All-Milestones-Batch-9/Milestone-6_intermediate-Javascript_API/JS-Sync-Async/JS/Sync-Async.js
+++ All-Milestones-Batch-9/Milestone-6_intermediate-Javascript_API/JS-Sync-Async/JS/Sync-Async.js
@@ -10,30 +10,30 @@ h1.addEventListener("click", function () {
   }
 });
 
-console.log("Hello 1");
-setTimeout(() => {
-  for (let i = 0; i < 10; i++) {
-    console.log(i);
-  }
-}, 0); // this function will execute after every synchronous code execution.( No metter the time is out or not)
-for (let i = 0; i < 100000; i++) {
-  console.log(i);
-}
-for (let i = 0; i < 100; i++) {
-  console.log("i am 3");
-}
+// console.log("Hello 1");
+// setTimeout(() => {
+//   for (let i = 0; i < 10; i++) {
+//     console.log(i);
+//   }
+// }, 0); // this function will execute after every synchronous code execution.( No metter the time is out or not)
+// for (let i = 0; i < 100000; i++) {
+//   console.log(i);
+// }
+// for (let i = 0; i < 100; i++) {
+//   console.log("i am 3");
+// }
 
-console.log("Hello 3");
+// console.log("Hello 3");
 
-const myItem = "Hello World!";
-function example(callback) {
-  callback();
-}
+// const myItem = "Hello World!";
+// function example(callback) {
+//   callback();
+// }
 
-function displayItem(item) {
-  console.log(item);
-}
+// function displayItem(item) {
+//   console.log(item);
+// }
 
-example(function () {
-  displayItem(myItem);
-});
+// example(function () {
+//   displayItem(myItem);
+// });
