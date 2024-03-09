559f2502a78c684ac264932797cf868f29361848
farhan-mazumder <farhanmazumder75@gmail.com>
2024-03-09 10:43:28 +0600

    module-38 explore-react-core-concepts practices has been added.

---
+++ All-Milestones-Batch-9/Milestone-7_Introduction-to-simple-react/module-38/my-react-app/src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <h1 className="text-center font-bold text-5xl h-screen flex items-center justify-center text-blue-400">
+        Hello World!
+      </h1>
+    </>
+  );
+}
+
+export default App;
